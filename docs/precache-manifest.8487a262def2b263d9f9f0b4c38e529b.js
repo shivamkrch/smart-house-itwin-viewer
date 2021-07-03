ad1245d8d48ca0b49a0b2dc48de0ea34b254be22
Shivam.Kumar <Shivam.Kumar@bentley.com>
2021-07-03 21:06:14 +0530

    Created prod build

---
+++ docs/precache-manifest.8487a262def2b263d9f9f0b4c38e529b.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad44ba2aca793e7480a84cbb5e579211",
+    "url": "/Markup/rotate.png"
+  },
+  {
+    "revision": "5316932e736aec50f9b79014d3baa8f0",
+    "url": "/assets/MapLayerSources.json"
+  },
+  {
+    "revision": "5b8c6acc72f6682347985ec28457c0ff",
+    "url": "/assets/approximateTerrainHeights.json"
+  },
+  {
+    "revision": "e0f66d0ff97ec7be9adf1f2d0dad48da",
+    "url": "/cursors/closedHand.cur"
+  },
+  {
+    "revision": "8af750ed503b5a9854105423018f16ab",
+    "url": "/cursors/crosshair.cur"
+  },
+  {
+    "revision": "22454cde40acb2b2d3c241f51dd239a7",
+    "url": "/cursors/dynamics.cur"
+  },
+  {
+    "revision": "2e4a3b99a23b364ea2e49fff0da65924",
+    "url": "/cursors/look.cur"
+  },
+  {
+    "revision": "ef50ac9e93aaebe3299791c79f277f8e",
+    "url": "/cursors/openHand.cur"
+  },
+  {
+    "revision": "578a25bbfdbb575018fab86e7aab044c",
+    "url": "/cursors/rotate.cur"
+  },
+  {
+    "revision": "0d4d6125f943717c354e8acf4e767ed5",
+    "url": "/cursors/walk.cur"
+  },
+  {
+    "revision": "2b1574473f59d8dcd8ef0e6faddf8f9f",
+    "url": "/cursors/zoom.cur"
+  },
+  {
+    "revision": "3ad2d1d002a8a48251f18005c7194335",
+    "url": "/images/about-imodeljs.svg"
+  },
+  {
+    "revision": "afe1c552d888f1d07b10a5c590253790",
+    "url": "/images/bing.svg"
+  },
+  {
+    "revision": "5a3c17bcdbbfa0c1afe55323aa9cd4e3",
+    "url": "/images/cesium-ion.svg"
+  },
+  {
+    "revision": "c13ebdf7b7378f43bf09d1c55abd6fd2",
+    "url": "/images/imodeljs-icon.svg"
+  },
+  {
+    "revision": "d6a23b9a8713b8b2db58430bc4c4daf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55572e749621bbea4f7e0ad2531415ac",
+    "url": "/locales/en-PSEUDO/BisCore.json"
+  },
+  {
+    "revision": "85b06d9eeaa91972fc4d516c5141c426",
+    "url": "/locales/en-PSEUDO/CoreTools.json"
+  },
+  {
+    "revision": "bce731db200d84279162316bb340b6c1",
+    "url": "/locales/en-PSEUDO/ECPresentation.json"
+  },
+  {
+    "revision": "a02681e1935e6e1953aa6c25e7c7898b",
+    "url": "/locales/en-PSEUDO/MarkupTools.json"
+  },
+  {
+    "revision": "fb08b5d14ee203c596d9de5e66645f18",
+    "url": "/locales/en-PSEUDO/PresentationComponents.json"
+  },
+  {
+    "revision": "eb562fa1f8ec2c2f2110c0feec069822",
+    "url": "/locales/en-PSEUDO/PropertyGrid.json"
+  },
+  {
+    "revision": "859a5978c2257d6088012bfed53ab675",
+    "url": "/locales/en-PSEUDO/RulesEngine.json"
+  },
+  {
+    "revision": "181d86dae14483a7496f71baa1bebd02",
+    "url": "/locales/en-PSEUDO/TreeWidget.json"
+  },
+  {
+    "revision": "2d23c2f9c5ef128a95527032dd2bbee7",
+    "url": "/locales/en-PSEUDO/UiAbstract.json"
+  },
+  {
+    "revision": "f92d75bba122d060f051ceab6b261a24",
+    "url": "/locales/en-PSEUDO/UiComponents.json"
+  },
+  {
+    "revision": "31064e3279dd25b6df84cae072423c97",
+    "url": "/locales/en-PSEUDO/UiCore.json"
+  },
+  {
+    "revision": "f7c33e3db0554359d42a0a85a9fb7c11",
+    "url": "/locales/en-PSEUDO/UiFramework.json"
+  },
+  {
+    "revision": "4d756d5893f466af62370eb41e1622de",
+    "url": "/locales/en-PSEUDO/iModelJs.json"
+  },
+  {
+    "revision": "df054b049471cd357ca4cd4780ea8533",
+    "url": "/locales/en/BisCore.json"
+  },
+  {
+    "revision": "3ebaabec25ea25ed380c5f61ba61fc25",
+    "url": "/locales/en/CoreTools.json"
+  },
+  {
+    "revision": "dec01db9f801552855cf314bfa1785bc",
+    "url": "/locales/en/ECPresentation.json"
+  },
+  {
+    "revision": "5a28ad8130a431ad01d2c66996243d67",
+    "url": "/locales/en/MarkupTools.json"
+  },
+  {
+    "revision": "bad41c1b9fd885b86121d78c7272d0de",
+    "url": "/locales/en/PresentationComponents.json"
+  },
+  {
+    "revision": "3da7050c12e29a93f2b54081d09fa7ce",
+    "url": "/locales/en/PropertyGrid.json"
+  },
+  {
+    "revision": "1f66b4553ef9a943dfe0a0960e05c331",
+    "url": "/locales/en/RulesEngine.json"
+  },
+  {
+    "revision": "6b8d9a0fdb88e3c7a188cf9c26ab1fae",
+    "url": "/locales/en/TreeWidget.json"
+  },
+  {
+    "revision": "f9ecd8ef6f94f3a5fafbc2fd07341b12",
+    "url": "/locales/en/UiAbstract.json"
+  },
+  {
+    "revision": "cf5f2108c20fc8db4adecd64caba7110",
+    "url": "/locales/en/UiComponents.json"
+  },
+  {
+    "revision": "f8d79a07e03e79b99b9468157bd1a8e0",
+    "url": "/locales/en/UiCore.json"
+  },
+  {
+    "revision": "7891420a60466d289cbb920d8e4b44ce",
+    "url": "/locales/en/UiFramework.json"
+  },
+  {
+    "revision": "f3709ff4f835e5e34e24739ae1aa56be",
+    "url": "/locales/en/iModelJs.json"
+  },
+  {
+    "revision": "a57937ae96a7336d9a18338b7622ca48",
+    "url": "/locales/en/iTwinViewer.json"
+  },
+  {
+    "revision": "12b6a6bb3223af1a0c7c1a6a7dfc3a76",
+    "url": "/scripts/checkbrowser.js"
+  },
+  {
+    "revision": "ef4bb46f11638fe8ba72eb1214eba2ed",
+    "url": "/sprites/SnapAppFiltered.png"
+  },
+  {
+    "revision": "c00f5a6c13c74a3ae41d353fc2dfc9cd",
+    "url": "/sprites/SnapBisector.png"
+  },
+  {
+    "revision": "5b65207e195f87de6f2fbc73427c9f0a",
+    "url": "/sprites/SnapCenter.png"
+  },
+  {
+    "revision": "95f5a4470d8663b955b264dded259d09",
+    "url": "/sprites/SnapCross.png"
+  },
+  {
+    "revision": "fbf9b630e8aac0070b961698741f5813",
+    "url": "/sprites/SnapIntersection.png"
+  },
+  {
+    "revision": "56e470096fa897c80bbfd1f9977a12bf",
+    "url": "/sprites/SnapKeypoint.png"
+  },
+  {
+    "revision": "4ef0edc6c76182a3f35e605e48f30408",
+    "url": "/sprites/SnapMidpoint.png"
+  },
+  {
+    "revision": "886005d51e6544060580b797073dd124",
+    "url": "/sprites/SnapNotSnappable.png"
+  },
+  {
+    "revision": "a3dba5e7527a20df4df3744bf501800b",
+    "url": "/sprites/SnapOrigin.png"
+  },
+  {
+    "revision": "e3ce3252873ceff0e8fcef3b41a390b4",
+    "url": "/sprites/SnapPointOn.png"
+  },
+  {
+    "revision": "6b6a0c32130e634ff24d7edfa500c37e",
+    "url": "/sprites/SnapUnfocused.png"
+  },
+  {
+    "revision": "38065e439da9af10b377",
+    "url": "/static/css/2.e621732f.chunk.css"
+  },
+  {
+    "revision": "b5f1a01789260dd32187",
+    "url": "/static/css/main.1ff9812f.chunk.css"
+  },
+  {
+    "revision": "38065e439da9af10b377",
+    "url": "/static/js/2.beb7088b.chunk.js"
+  },
+  {
+    "revision": "1f016f578cdfb4f24dcd6fc53be62f5a",
+    "url": "/static/js/2.beb7088b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f1a01789260dd32187",
+    "url": "/static/js/main.b68a788f.chunk.js"
+  },
+  {
+    "revision": "23eefc304394251c6194",
+    "url": "/static/js/runtime-main.7265e954.js"
+  },
+  {
+    "revision": "56e022d085b98c223e424d03e59e4b83",
+    "url": "/static/media/401-9ZUI-WEB.56e022d0.svg"
+  },
+  {
+    "revision": "7bcbb37570a69a72f5b91d5d4e3a01a2",
+    "url": "/static/media/403-9ZUI-WEB.7bcbb375.svg"
+  },
+  {
+    "revision": "6674307ca12be5c6897e484c4ce8b577",
+    "url": "/static/media/404-WEB.6674307c.svg"
+  },
+  {
+    "revision": "e0f6a9ce78f2c274e8bbf362696c8b88",
+    "url": "/static/media/500-WEB.e0f6a9ce.svg"
+  },
+  {
+    "revision": "59c0c59fd6611b2a6b6b26efa835d4f8",
+    "url": "/static/media/503-WEB.59c0c59f.svg"
+  },
+  {
+    "revision": "f7c75cda724396ef202aa24a595c976b",
+    "url": "/static/media/Generic-WEB.f7c75cda.svg"
+  },
+  {
+    "revision": "450271551e597c8b23c903db3e2a765e",
+    "url": "/static/media/bentley-icons-generic-webfont.45027155.ttf"
+  },
+  {
+    "revision": "62f83a7ab5b95e582098ebfad8cc6e04",
+    "url": "/static/media/bentley-icons-generic-webfont.62f83a7a.eot"
+  },
+  {
+    "revision": "65a7dd1c4bec564ae82aa3b1ad97b124",
+    "url": "/static/media/bentley-icons-generic-webfont.65a7dd1c.woff2"
+  },
+  {
+    "revision": "ebe971e60cce154b01e2a74c89ff674a",
+    "url": "/static/media/bentley-icons-generic-webfont.ebe971e6.svg"
+  },
+  {
+    "revision": "fbd8ab0287e6e463c0178cca87782307",
+    "url": "/static/media/bentley-icons-generic-webfont.fbd8ab02.woff"
+  }
+]);
